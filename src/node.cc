c6ec32e39d7a3a0eda070c2a99c2dbd31a47e39b
Kelvin Jin <kelvinjin@google.com>
2019-02-26 16:12:04 -0800

    trace_event: add perfetto

--- src/node.cc
+++ src/node.cc
@@ -48,6 +48,10 @@
 #include "inspector_io.h"
 #endif
 
+#if HAVE_PERFETTO
+#include "node_perfetto_tracing.h"
+#endif
+
 #if defined HAVE_DTRACE || defined HAVE_ETW
 #include "node_dtrace.h"
 #endif
@@ -768,6 +772,10 @@ inline int Start(Isolate* isolate, IsolateData* isolate_data,
                                       Environment::kOwnsInspector));
   env.Start(per_process::v8_is_profiling);
   env.ProcessCliArgs(args, exec_args);
+
+#if HAVE_PERFETTO
+  GetNodeTracing().Initialize();
+#endif
 
 #if HAVE_INSPECTOR && NODE_USE_V8_PLATFORM
   CHECK(!env.inspector_agent()->IsListening());
